ce67e26a22a3f9447e2fa817db88a442b183df42
didrik <didrik.sommersten@gmail.com>
2023-08-08 23:48:17 +0200

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,42 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let word = ""; // Your 5-letter word
+  let numRows = 6;    // Number of rows
+  let placeholder = "";
+  let currentRow = 0;
+  let rowTexts = Array.from({ length: numRows }, () => "");
+
+  window.addEventListener('keydown', event => {
+    if (event.key.length === 1 && /^[a-æøåA-ÆØÅ]+$/.test(event.key) && word.length < 5) {
+      word += event.key;
+      rowTexts[currentRow] = word;
+    } else if (event.key === "Backspace") {
+      word = word.slice(0, -1);
+      rowTexts[currentRow] = word;
+    } else if (event.key === "Enter") {
+      currentRow = (currentRow + 1) % numRows;
+      word = rowTexts[currentRow] || "";
+    }
+  });
+</script>
+
+<div class="header">
+  <h1 class="title">NORSK WORDLE</h1>
+</div>
+
+<div class="center">
+  <div class="grid">
+    {#each Array.from({ length: numRows }) as _, rowIndex}
+      <div class="row">
+        {#each Array.from({ length: 5 }) as _, boxIndex}
+          <div class="box">
+            {#if rowTexts[rowIndex] && boxIndex < rowTexts[rowIndex].length}
+              {rowTexts[rowIndex][boxIndex].toUpperCase()}
+            {:else}
+              {placeholder}
+            {/if}
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
+</div>
