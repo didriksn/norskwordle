5c5dbc71a4050f73ecce732775eacd30edca27ea
didrik <didrik.sommersten@gmail.com>
2023-08-17 10:22:22 +0200

    fixed not able to press buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -259,20 +259,20 @@
 <div id="keyboard-cont">
   <div class="first-row">
     {#each firstRowLetters as lett}
-      <button class="keyboard-button" style="background-color:{wordObj[lett] == '#6AAA64' ? '#6AAA64' : wordObj[lett]}; transition-delay: {offsetDelay}ms; transition-property: color, background-color; color: {wordObj[lett] !== '#d3d6da' ? 'white' : 'black'};" on:click={() => handleButtonClick({lett})}>{lett}</button>
+      <button class="keyboard-button" style="background-color:{wordObj[lett] == '#6AAA64' ? '#6AAA64' : wordObj[lett]}; transition-delay: {offsetDelay}ms; transition-property: color, background-color; color: {wordObj[lett] !== '#d3d6da' ? 'white' : 'black'};" on:click={() => handleButtonClick(lett)}>{lett}</button>
     {/each}
   </div>
 
   <div class="second-row">
     {#each secondRowLetters as lett}
-      <button class="keyboard-button" style="background-color:{wordObj[lett] == '#6AAA64' ? '#6AAA64' : wordObj[lett]}; transition-delay: {offsetDelay}ms; transition-property: color, background-color; color: {wordObj[lett] !== '#d3d6da' ? 'white' : 'black'};" on:click={() => handleButtonClick({lett})}>{lett}</button>
+      <button class="keyboard-button" style="background-color:{wordObj[lett] == '#6AAA64' ? '#6AAA64' : wordObj[lett]}; transition-delay: {offsetDelay}ms; transition-property: color, background-color; color: {wordObj[lett] !== '#d3d6da' ? 'white' : 'black'};" on:click={() => handleButtonClick(lett)}>{lett}</button>
     {/each}
   </div>
 
   <div class="third-row">
     <button class="keyboard-button enter" on:click={() => handleButtonClick("Enter")}>Enter</button>
     {#each thirdRowLetters as lett}
-      <button class="keyboard-button" style="background-color:{wordObj[lett] == '#6AAA64' ? '#6AAA64' : wordObj[lett]}; transition-delay: {offsetDelay}ms; transition-property: color, background-color; color: {wordObj[lett] !== '#d3d6da' ? 'white' : 'black'};" on:click={() => handleButtonClick({lett})}>{lett}</button>
+      <button class="keyboard-button" style="background-color:{wordObj[lett] == '#6AAA64' ? '#6AAA64' : wordObj[lett]}; transition-delay: {offsetDelay}ms; transition-property: color, background-color; color: {wordObj[lett] !== '#d3d6da' ? 'white' : 'black'};" on:click={() => handleButtonClick(lett)}>{lett}</button>
     {/each}
     <button class="keyboard-button enter" on:click={() => handleButtonClick('Backspace')}><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 24 24"><path d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path></svg></button>
   </div>
