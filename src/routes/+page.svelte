00986a092817c8e5dc35e3e1f03ba857816af283
didriksn <102746612+didriksn@users.noreply.github.com>
2025-11-11 14:31:44 +0100

    Simplify date calculation by removing offset
    Removed timezone offset from date calculation.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -72,7 +72,7 @@
   Å: "#d3d6da"
 };
 
-  const offsetFromDate = new Date(Date.UTC(2025, 6, 27, 0, 0, 0, 0) + (2 + 60 * 60 * 1000));
+  const offsetFromDate = new Date(Date.UTC(2025, 6, 27, 0, 0, 0, 0));
   const msOffset = Date.now() - offsetFromDate;
   const dayOffset = msOffset / 1000 / 60 / 60 / 24;
   let correctWord = betterWords[Math.floor(dayOffset)]
